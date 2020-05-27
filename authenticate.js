e35af3336accfd4c84e45fb06bc658563b7558d7
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2020-05-27 19:13:32 -0400

    Commenting

--- authenticate.js
+++ authenticate.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const errh = require('./error_handling');
 
 function check_login(req, body){
-    if (req.method == "POST"){
-        //const login_details = fs.readFileSync('./logins.json');
-        //const keys = Object.keys(login_details);
+    if (req.method == "POST"){ // make sure that the http method is correct
         const submitted = body.split('&');
+        // the next 3 lines check to make sure that form data was properly encoded
         if (submitted.length == 2){
             if (submitted[0].includes('=') && submitted[1].includes('=')) {
                 if (submitted[0].split('=')[0] == 'uname' && submitted[1].split('=')[0] == 'pword'){
@@ -13,16 +12,10 @@ function check_login(req, body){
                         const uname =  submitted[0].split('=')[1];
                         const pword =  submitted[1].split('=')[1];
                         const login_details = JSON.parse(fs.readFileSync('./logins.json', {encoding: 'utf-8'}));
-                        //const keys = Object.keys(login_details);
-                        //console.log(uname);
-                        //console.log(pword);
-                        //console.log(keys);
-                        console.log(typeof(login_details));
-                        if (!(login_details.hasOwnProperty(uname))){
-                            //console.log(login_details[uname]);
+                        if (!(login_details.hasOwnProperty(uname))){ // does the username exist?
                             return {code: 401, data: errh.get_error_file(401)}
                         } else if (login_details[uname] != pword) {
-                            console.log(login_details[uname])
+                            // is the submitted password the same as the password listed in logins.json?
                             return {code: 401, data: errh.get_error_file(401)}
                         } else {
                             return {code: 200, data: 'Success!'}
@@ -34,15 +27,11 @@ function check_login(req, body){
                     return {code: 400, data: errh.get_error_file(400)}
                 }
             } else {
-                //console.log("= not in params");
                 return {code: 400, data: errh.get_error_file(400)}
             }
         } else {
-            //console.log("Incorrect length");
             return {code: 400, data: errh.get_error_file(400)}
         }
-        //console.log(body);
-        //return {code: 200, data: "Parsed data successfully"}
     } else {
         return {code: 405, data: errh.get_error_file(405)}
     }
